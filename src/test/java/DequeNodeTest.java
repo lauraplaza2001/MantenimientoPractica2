0d27cdcdd13f97a2f6b2261a9b567152eead90bf
LauraPlaza <0619967435@uma.es>
2022-03-17 11:33:18 +0100

    compienzo testDEque

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,5 +1,42 @@
+import org.example.DequeNode;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class DequeNodeTest {
+class DequeNodeTest<T> {
+    private T item;
+    private DequeNode<T> next;
+    private DequeNode<T> previous;
+
+    @BeforeEach
+    public void setUp(){
+        next = null;
+        previous= null;
+        item = null;
+    }
+
+    public void finish(){
+        next = null;
+        previous = null;
+        item = null;
+    }
+
+
+    @Test
+    public void testGetItemReturnItem(){
+        assertEquals(item, );
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
