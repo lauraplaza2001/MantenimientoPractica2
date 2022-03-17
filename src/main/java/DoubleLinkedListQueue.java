ab2f0a2d9b0e0ce23f362324b77b2fe066b85fed
cberdejo <cberdejo@uma.es>
2022-03-17 11:48:05 +0100

    Principio de lista doblemente enlazada

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -1,4 +1,49 @@
 import org.example.DoubleEndedQueue;
 
-public class DoubleLinkedListQueue implements DoubleEndedQueue {
+
+public class DoubleLinkedListQueue <T> implements DoubleEndedQueue {
+
+    private DequeNode [] Nodes;
+
+    public DoubleLinkedListQueue(T item){
+        if (item == null) throw new RuntimeException("Not null item");
+        DequeNode node = new DequeNode(item,null,null); //first Node in the list
+        this.append(node);
+
+    }
+
+    @Override
+    public void append(DequeNode node) {
+
+    }
+
+    @Override
+    public void appendLeft(DequeNode node) {
+
+    }
+
+    @Override
+    public void deleteFirst() {
+
+    }
+
+    @Override
+    public void deleteLast() {
+
+    }
+
+    @Override
+    public DequeNode peekFirst() {
+        return null;
+    }
+
+    @Override
+    public DequeNode peekLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
