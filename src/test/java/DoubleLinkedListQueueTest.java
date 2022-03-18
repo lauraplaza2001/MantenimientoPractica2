351439fd53977124e7d22b3024f61eb37b1ff4cc
LauraPlaza <0619967435@uma.es>
2022-03-18 11:36:11 +0100

    double linked list test beggining

--- src/test/java/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleLinkedListQueueTest.java
@@ -1,5 +1,38 @@
+import org.example.DequeNode;
+import org.example.DoubleLinkedListQueue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleLinkedListQueueTest {
+   DoubleLinkedListQueue queue ;
+
+    @BeforeAll
+    public void setUp(){
+        queue = new DoubleLinkedListQueue<>();
+
+    }
+    public  void finish(){
+        queue = null;
+    }
+
+    @Test
+    public void testSizeReturnZeroIfEmptyQueue(){
+        DoubleLinkedListQueue queue = new DoubleLinkedListQueue();
+        int expected =0;
+        int obtained = queue.size();
+        assertEquals(expected,obtained);
+    }
+
+    @Test
+    public void testSizeReturnTheSizeIfNotEmptyQueue(){
+        DoubleLinkedListQueue queue = new DoubleLinkedListQueue();
+        queue.append(new DequeNode(3,null,null));
+        int expected = 1;
+        int obtained= queue.size();
+        assertEquals(obtained,expected);
+    }
+
 
 }
